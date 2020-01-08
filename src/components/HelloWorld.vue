f12337ae470ff9a14f371e046209221f7810c38f
Filip Wójtowicz <fil.wojtowicz@gmail.com>
2020-01-08 20:06:07 +0100

    sandbox #1

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,36 +1,58 @@
 <template>
   <div class="hello">
-    <div v-for="person in allPeople.people" :key="person.name">{{person.name}}</div>
+    <!-- <div v-for="person in allPeople.people" :key="person.name">{{person.name}}</div> -->
+
+    <!-- <ApolloQuery
+      :query="gql => gql `query {
+    allPeople {
+    people {
+    name
+    }
+    }
+    }`"
+    >
+     <template v-slot="{result: {loading, error, allPeople}}"></template>
+      <div v-if="allPeople">
+        <div v-for="person in allPeople.people" :key="person.name">{{person.name}}</div>
+      </div>
+    </ApolloQuery>-->
+    <ApolloQuery :query="require('../graphql/allCharacters.gql')" :variables="{ first: 5 }">
+      <template v-slot="{result: {loading, error, data}}">
+        <div v-if="data">
+          <div v-for="person in data.allPeople.people" :key="person.name">{{person.name}}</div>
+        </div>
+      </template>
+    </ApolloQuery>
   </div>
 </template>
 
 <script>
-import gql from "graphql-tag";
+// import gql from "graphql-tag";
+
 export default {
   name: "HelloWorld",
   data() {
     return {
-      first: 10
+      first: 2
     };
   },
   props: {
     msg: String
-  },
-  apollo: {
-    allPeople: gql`
-      query {
-        allPeople {
-          people {
-            name
-          }
-        }
-      }
-    `
   }
+  // apollo: {
+  //   allPeople: gql`
+  //     query {
+  //       allPeople {
+  //         people {
+  //           name
+  //         }
+  //       }
+  //     }
+  //   `
+  // }
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h3 {
   margin: 40px 0 0;
